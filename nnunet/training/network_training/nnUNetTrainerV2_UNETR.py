db3713270e6137932feda7085e1bb65ae9af5ff1
SUSY MAIJER <susymayer1996@gmail.com>
2022-06-14 21:54:52 +0200

    Add init to trainer

--- nnunet/training/network_training/nnUNetTrainerV2_UNETR.py
+++ nnunet/training/network_training/nnUNetTrainerV2_UNETR.py
@@ -22,6 +22,11 @@ from time import time
 
 class nnUNetTrainerV2_UNETR(nnUNetTrainerV2):
 
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
+                         
     def initialize_network(self):
         self.print_to_log_file("UNETR initialising network")
         self.network = UNETR(self.num_input_channels, self.num_classes, self.patch_size, self.net_pool_per_axis,
