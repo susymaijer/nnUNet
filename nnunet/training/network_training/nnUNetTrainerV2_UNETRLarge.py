2956b680d9c9e2d013e4135966c45453b56d4508
SUSY MAIJER <susymayer1996@gmail.com>
2022-06-15 13:35:44 +0200

    Make large UNETR

--- nnunet/training/network_training/nnUNetTrainerV2_UNETRLarge.py
+++ nnunet/training/network_training/nnUNetTrainerV2_UNETRLarge.py
@@ -13,13 +13,11 @@
 #    limitations under the License.
 import torch
 from nnunet.network_architecture.unetr.unetr import UNETR
-from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
-from nnunet.utilities.nd_softmax import softmax_helper
+from nnunet.training.network_training.nnUNetTrainerV2_UNETR import nnUNetTrainerV2_UNETR
 
-class nnUNetTrainerV2_UNETR(nnUNetTrainerV2):
+class nnUNetTrainerV2_UNETRLarge(nnUNetTrainerV2_UNETR):
 
-    ''' Classical UNETR ''''
-
+    ''' Classical UNETR, except that the feature size is 32 (=same to nnUnet) instead of 16. '''
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
@@ -28,14 +26,7 @@ class nnUNetTrainerV2_UNETR(nnUNetTrainerV2):
     def initialize_network(self):
         self.print_to_log_file("UNETR initialising network")
         self.network = UNETR(self.num_input_channels, self.num_classes, self.patch_size, self.net_pool_per_axis,
-                                len(self.net_num_pool_op_kernel_sizes), self.net_num_pool_op_kernel_sizes, do_print=False)
+                                len(self.net_num_pool_op_kernel_sizes), self.net_num_pool_op_kernel_sizes, do_print=False,
+                                feature_size = 32)
         if torch.cuda.is_available():
             self.network.cuda()
-
-    def load_checkpoint(self, fname, train=True):
-        self.print_to_log_file("UNETR loading checkpoint", fname, "train=", train)
-        if not self.was_initialized:
-            self.initialize(train)
-        # saved_model = torch.load(fname, map_location=torch.device('cuda', torch.cuda.current_device()))
-        saved_model = torch.load(fname, map_location=torch.device('cpu'))
-        self.load_checkpoint_ram(saved_model, train)
