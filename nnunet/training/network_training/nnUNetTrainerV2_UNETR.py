3bf2ae8f3961d4bdaf9ce1fb3fa84379af09e60b
SUSY MAIJER <susymayer1996@gmail.com>
2022-06-17 08:23:30 +0200

    wtf..

--- nnunet/training/network_training/nnUNetTrainerV2_UNETR.py
+++ nnunet/training/network_training/nnUNetTrainerV2_UNETR.py
@@ -25,7 +25,6 @@ class nnUNetTrainerV2_UNETR(nnUNetTrainerV2):
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
-        self.conv_op == nn.Conv3d # TODO do dynamic based o input
 
     def initialize_network(self):
         self.print_to_log_file("UNETR initialising network")
