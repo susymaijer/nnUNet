309c5990c7607d43f2664c71137134628069b0a9
SUSY MAIJER <susymayer1996@gmail.com>
2022-06-17 07:54:12 +0200

    print

--- nnunet/network_architecture/neural_network.py
+++ nnunet/network_architecture/neural_network.py
@@ -141,6 +141,7 @@ class SegmentationNetwork(NeuralNetwork):
         with context():
             with torch.no_grad():
                 if self.conv_op == nn.Conv3d:
+                    print("Suus 3d")
                     if use_sliding_window:
                         res = self._internal_predict_3D_3Dconv_tiled(x, step_size, do_mirroring, mirror_axes, patch_size,
                                                                      regions_class_order, use_gaussian, pad_border_mode,
@@ -150,6 +151,7 @@ class SegmentationNetwork(NeuralNetwork):
                         res = self._internal_predict_3D_3Dconv(x, patch_size, do_mirroring, mirror_axes, regions_class_order,
                                                                pad_border_mode, pad_kwargs=pad_kwargs, verbose=verbose)
                 elif self.conv_op == nn.Conv2d:
+                    print("Suus2d")
                     if use_sliding_window:
                         res = self._internal_predict_3D_2Dconv_tiled(x, patch_size, do_mirroring, mirror_axes, step_size,
                                                                      regions_class_order, use_gaussian, pad_border_mode,
