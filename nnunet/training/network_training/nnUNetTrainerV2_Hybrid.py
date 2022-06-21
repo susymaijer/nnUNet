1ff2ff0f4ce4802c8b87b7db9613bf6b2aa6d85f
SUSY MAIJER <susymayer1996@gmail.com>
2022-06-21 09:21:58 +0200

    fix epochs and printing

--- nnunet/training/network_training/nnUNetTrainerV2_Hybrid.py
+++ nnunet/training/network_training/nnUNetTrainerV2_Hybrid.py
@@ -62,7 +62,7 @@ class nnUNetTrainerV2_Hybrid(nnUNetTrainerV2):
                                 pool_op_kernel_sizes=self.net_num_pool_op_kernel_sizes,
                                 upscale_logits=False, 
                                 convolutional_upsampling=True,
-                                do_print=True # till here its u-net
+                                do_print=False # till here its u-net
                                     )
         if torch.cuda.is_available():
             self.network.cuda()
