ff3dc9f1d9ded4abc69518a7690ca80d392a4f31
SUSY MAIJER <susymayer1996@gmail.com>
2022-07-05 19:09:39 +0200

    fix stupid mistake

--- nnunet/postprocessing/consolidate_postprocessing_simple.py
+++ nnunet/postprocessing/consolidate_postprocessing_simple.py
@@ -55,7 +55,7 @@ def main():
 
     folder = get_output_folder_name(model, task, trainer, plans, None)
 
-    consolidate_folds(folder, val, fold=folds)
+    consolidate_folds(folder, val, folds=folds)
 
 
 if __name__ == "__main__":
