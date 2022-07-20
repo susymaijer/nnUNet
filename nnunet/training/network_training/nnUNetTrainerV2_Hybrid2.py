7e388f73c7a4da4af5b33360b3f2ae1fada53360
SUSY MAIJER <susymayer1996@gmail.com>
2022-07-20 15:39:20 +0200

    Add hybrid2 with unetr optimizer

---
+++ nnunet/training/network_training/nnUNetTrainerV2_Hybrid2.py
@@ -0,0 +1,30 @@
+#    Copyright 2020 Division of Medical Image Computing, German Cancer Research Center (DKFZ), Heidelberg, Germany
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import torch
+from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2_Hybrid
+
+class nnUNetTrainerV2_Hybrid2(nnUNetTrainerV2_Hybrid):
+
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
+        self.initial_lr = 1e-4
+        self.weight_decay = 1e-5
+
+    def initialize_optimizer_and_scheduler(self):
+        assert self.network is not None, "self.initialize_network must be called first"
+        self.optimizer = torch.optim.AdamW(self.network.parameters(), lr=self.initial_lr, weight_decay=self.weight_decay)
+        self.lr_scheduler = None
