cd63d954b0b3a5b3a8303625d59a57357994c2be
SUSY MAIJER <susymayer1996@gmail.com>
2022-08-10 13:20:37 +0200

    Definite 0.1 weights for other

--- nnunet/training/network_training/nnUNet_variants/loss_function/nnUNetTrainerV2_Loss_DC_CE_weight01.py
+++ nnunet/training/network_training/nnUNet_variants/loss_function/nnUNetTrainerV2_Loss_DC_CE_weight01.py
@@ -18,11 +18,12 @@ from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
 from nnunet.training.loss_functions.dice_loss import weighted_DC_and_CE_loss
 
 
-class nnUNetTrainerV2_Loss_DC_CE_weight1(nnUNetTrainerV2):
+class nnUNetTrainerV2_Loss_DC_CE_weight01(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
         
-        weights=None#weights = torch.tensor(1)
+        weights = torch.empty(self.num_classes+1).fill_(0.1)
+        weights[1] = 1 # pancreas label is always 1
         self.loss = weighted_DC_and_CE_loss({'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False}, {}, label_weights = weights)
