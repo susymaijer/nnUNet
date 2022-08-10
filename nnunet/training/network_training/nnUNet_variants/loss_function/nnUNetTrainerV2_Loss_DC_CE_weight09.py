ea0bd94ab98245752b21a57107aecf921ee909d8
SUSY MAIJER <susymayer1996@gmail.com>
2022-08-10 17:18:00 +0200

    Add weightings 0.5 and 0.9 as well

--- nnunet/training/network_training/nnUNet_variants/loss_function/nnUNetTrainerV2_Loss_DC_CE_weight09.py
+++ nnunet/training/network_training/nnUNet_variants/loss_function/nnUNetTrainerV2_Loss_DC_CE_weight09.py
@@ -13,22 +13,11 @@
 #    limitations under the License.
 
 
-import torch
-from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
-from nnunet.training.loss_functions.dice_loss import weighted_DC_and_CE_loss
+from nnunet.training.network_training.nnUNet_variants.loss_function.nnUNetTrainerV2_Loss_DC_CE_weight01 import nnUNetTrainerV2_Loss_DC_CE_weight01
 
-
-class nnUNetTrainerV2_Loss_DC_CE_weight01(nnUNetTrainerV2):
+class nnUNetTrainerV2_Loss_DC_CE_weight09(nnUNetTrainerV2_Loss_DC_CE_weight01):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
-        self.lower_weight = 0.1
-
-    def process_plans(self, plans):
-        super().process_plans(plans)
-        weights = torch.empty(self.num_classes).fill_(self.lower_weight)
-        weights[1] = 1 # pancreas label is always 1
-        if torch.cuda.is_available():
-            weights = weights.to(device="cuda:0")
-        self.loss = weighted_DC_and_CE_loss({'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False}, {}, label_weights = weights)
+        self.lower_weight = 0.9
