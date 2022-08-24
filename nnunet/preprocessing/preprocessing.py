db8131a28bfc7ae5284846952a99344c7ce48bfc
SUSY MAIJER <susymayer1996@gmail.com>
2022-08-24 11:02:59 +0200

    specific timing

--- nnunet/preprocessing/preprocessing.py
+++ nnunet/preprocessing/preprocessing.py
@@ -311,13 +311,20 @@ class GenericPreprocessor(object):
         return data, seg, properties
 
     def preprocess_test_case(self, data_files, target_spacing, seg_file=None, force_separate_z=None):
+        import time
+        t = time.time()
         data, seg, properties = ImageCropper.crop_from_list_of_files(data_files, seg_file)
+        t2 = time.time()
 
         data = data.transpose((0, *[i + 1 for i in self.transpose_forward]))
+        t3 = time.time()
         seg = seg.transpose((0, *[i + 1 for i in self.transpose_forward]))
+        t4 = time.time()
 
         data, seg, properties = self.resample_and_normalize(data, target_spacing, properties, seg,
                                                             force_separate_z=force_separate_z)
+        t5 = time.time() 
+        print(f"[Timing] Preprocessing specifics: cropping took {t2 - t}, transposing took {t3-t2} and {t4-t3}, resampling/normalizing took {t5-t4}.")
         return data.astype(np.float32), seg, properties
 
     def _run_internal(self, target_spacing, case_identifier, output_folder_stage, cropped_output_dir, force_separate_z,
